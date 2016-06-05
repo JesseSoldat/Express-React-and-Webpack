c798c8dafe97a75258e0365027c9f314596b46e2
jesse soldat <jsoldat@hotmail.com>
2016-06-05 21:00:06 +0700

    added header component

--- app-server.js
+++ app-server.js
@@ -6,6 +6,10 @@ app.use(express.static('./public'));
 
 app.use(express.static('./node_modules/bootstrap/dist'));
 
-app.listen(3000);
+var server = app.listen(3000);
+var io = require('socket.io').listen(server);
 
+io.sockets.on('connection', function(socket){
+	console.log(socket.id);
+});
 console.log('Polling server running at "http:localhost:3000"');
--- components/APP.js
+++ components/APP.js
@@ -1,9 +1,22 @@
 var React = require('react');
+var io = require('socket.io-client');
+var Header = require('./parts/Header');
 
 var APP = React.createClass({
+
+	componentWillMount() {
+		this.socket = io('http://localhost:3000');
+		this.socket.on('connect', this.connect);
+	},
+	connect(){
+		console.log('Connected ' + this.socket.id);
+	},
+
 	render() {
 		return (
-			<h1>Hello from React</h1>
+			<div>
+				<Header title="New Header" />
+			</div>
 			);
 	}
 });
