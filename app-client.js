be1126671c6364b5013f4b2bbf8927962d8fe368
jesse soldat <jsoldat@hotmail.com>
2016-06-06 13:26:38 +0700

    added audience speaker and board component

--- app-client.js
+++ app-client.js
@@ -1,5 +1,18 @@
 var React = require('react');
 var ReactDom = require('react-dom');
+
+
+
 var APP = require('./components/APP');
+var Audience = require('./components/audience');
+var Speaker = require('./components/speaker');
+var Board = require('./components/board');
+
+
+
+
 
 ReactDom.render(<APP />, document.getElementById('root'));
+
+
+
